f6b0c83d1fbf22c9b6b73cc6e767626f4a28064f
SforSuhaib <Suhaibbilal008@gmail.com>
2024-12-15 00:10:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-// Set the date and time we're counting down to: 12:05 PM on 15th December 2024
-var countDownDate = new Date("Dec 15, 2024 12:05:00").getTime();
+// Timer function to show countdown
+const targetDate = new Date("Dec 15, 2024 12:00:00").getTime();
+const timerElement = document.getElementById('timer');
+const specialButton = document.getElementById('specialButton');
 
-// Update the countdown every 1 second
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+// Update the countdown every second
+const interval = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
 
-    // Calculate time left
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the timer
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    timerElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     // If the countdown is over, show the button
     if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("specialButton").style.display = "block";
+        clearInterval(interval);
+        timerElement.innerHTML = "The wait is over!";
+        specialButton.style.display = "block"; // Make the button appear
     }
 }, 1000);
 
-// Event listener for button click
-document.getElementById("specialButton").addEventListener("click", function() {
-    // Hide the button after it's clicked
-    document.getElementById("specialButton").style.display = "none";
-    
-    // Show the image after the button is clicked
-    var img = document.createElement("img");
-    img.src = "letter.jpg"; // Make sure the image is in the same folder
-    img.alt = "Happy 2nd Anniversary!";
-    document.body.appendChild(img);
-});
+// Function to show the letter when button is clicked
+function showLetter() {
+    document.getElementById('letterContainer').style.display = "block"; // Show the letter
+    specialButton.style.display = "none"; // Hide the button after click
+}
