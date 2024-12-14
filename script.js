c4fcbd64239fb591e3deae2b0073f6958a1b09af
SforSuhaib <Suhaibbilal008@gmail.com>
2024-12-15 00:03:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 15, 2024 00:00:00").getTime();
+// Set the date and time we're counting down to: 12:05 PM on 15th December 2024
+var countDownDate = new Date("Dec 15, 2024 12:05:00").getTime();
 
 // Update the countdown every 1 second
 var x = setInterval(function() {
-
-    // Get the current time
     var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes, and seconds
+
+    // Calculate time left
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Display the countdown
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
 
-    // If the countdown is finished, show the button
+    // Display the timer
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the countdown is over, show the button
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
-        document.getElementById("special-btn-container").style.display = "block";
+        document.getElementById("specialButton").style.display = "block";
     }
 }, 1000);
 
-// Button click event to show the image after countdown
-document.getElementById("special-btn").onclick = function() {
-    document.getElementById("special-btn-container").style.display = "none";
-    document.getElementById("image-container").style.display = "block";
-};
-
+// Event listener for button click
+document.getElementById("specialButton").addEventListener("click", function() {
+    // Hide the button after it's clicked
+    document.getElementById("specialButton").style.display = "none";
+    
+    // Show the image after the button is clicked
+    var img = document.createElement("img");
+    img.src = "letter.jpg"; // Make sure the image is in the same folder
+    img.alt = "Happy 2nd Anniversary!";
+    document.body.appendChild(img);
+});
